c60ea3288cb2d8bc463e946e0c3024d93f0de007
skemnitz <kemnitz.stefan@googlemail.com>
2019-03-12 15:05:30 +0100

    restuctured into multiple headers

---
+++ Light.hpp
@@ -0,0 +1,12 @@
+
+#pragma once
+
+#include "geometry.h"
+
+struct Light {
+    Light(const Vec3f &p, const float i) : position(p), intensity(i) {}
+    Vec3f position;
+    float intensity;
+};
+
+
--- tinyraytracer.cpp
+++ tinyraytracer.cpp
@@ -7,41 +7,9 @@
 #include <algorithm>
 #include "geometry.h"
 
-struct Light {
-    Light(const Vec3f &p, const float i) : position(p), intensity(i) {}
-    Vec3f position;
-    float intensity;
-};
-
-struct Material {
-    Material(const float r, const Vec4f &a, const Vec3f &color, const float spec) : refractive_index(r), albedo(a), diffuse_color(color), specular_exponent(spec) {}
-    Material() : refractive_index(1), albedo(1,0,0,0), diffuse_color(), specular_exponent() {}
-    float refractive_index;
-    Vec4f albedo;
-    Vec3f diffuse_color;
-    float specular_exponent;
-};
-
-struct Sphere {
-    Vec3f center;
-    float radius;
-    Material material;
-
-    Sphere(const Vec3f &c, const float r, const Material &m) : center(c), radius(r), material(m) {}
-
-    bool ray_intersect(const Vec3f &orig, const Vec3f &dir, float &t0) const {
-        Vec3f L = center - orig;
-        float tca = L*dir;
-        float d2 = L*L - tca*tca;
-        if (d2 > radius*radius) return false;
-        float thc = sqrtf(radius*radius - d2);
-        t0       = tca - thc;
-        float t1 = tca + thc;
-        if (t0 < 0) t0 = t1;
-        if (t0 < 0) return false;
-        return true;
-    }
-};
+#include "Light.hpp"
+#include "Material.hpp"
+#include "Sphere.hpp"
 
 Vec3f reflect(const Vec3f &I, const Vec3f &N) {
     return I - N*2.f*(I*N);
@@ -119,7 +87,6 @@ void render(const std::vector<Sphere> &spheres, const std::vector<Light> &lights
     const float fov      = M_PI/3.;
     std::vector<Vec3f> framebuffer(width*height);
 
-    #pragma omp parallel for
     for (size_t j = 0; j<height; j++) { // actual rendering loop
         for (size_t i = 0; i<width; i++) {
             float dir_x =  (i + 0.5) -  width/2.;
@@ -140,7 +107,6 @@ void render(const std::vector<Sphere> &spheres, const std::vector<Light> &lights
             ofs << (char)(255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
         }
     }
-    ofs.close();
 }
 
 int main() {
